2b58b268ad214c74dab2acbaf134a5637c845a88
Little-Captain <littlecaptain@foxmail.com>
2017-04-30 11:19:51 +0800

    初始化项目

---
+++ QQMusic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QQMusic
+//
+//  Created by Liu-Mac on 4/30/17.
+//  Copyright © 2017 Liu-Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
